be9411b6d6a9ef86e01c9aa02358228fcb2b407d
Hendrik Liebau <mail@hendrik-liebau.de>
2025-05-06 10:57:31 +0200

    Rename `doNotThrowOnEmptyStaticShell` to `allowEmptyStaticShell`

--- packages/next/src/build/index.ts
+++ packages/next/src/build/index.ts
@@ -2682,8 +2682,7 @@ export default async function build(
                     _isDynamicError: isDynamicError,
                     _isAppDir: true,
                     _isRoutePPREnabled: isRoutePPREnabled,
-                    _doNotThrowOnEmptyStaticShell:
-                      !route.throwOnEmptyStaticShell,
+                    _allowEmptyStaticShell: !route.throwOnEmptyStaticShell,
                   }
                 })
               })
